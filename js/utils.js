439ed868fc1aa0cb22403504752dfab2a6a5ee90
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2023-04-20 00:24:50 -0600

    Add functions to encrypt and decrypt text

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,33 @@
 var forbbiden_words = [];
 
-function getText () {
-    let text = document.getElementById('text-area').value;
-    let words = text.split(' ');
-    console.log(words);
+const replacedLetters = (text, encrypt) => {
+    text = text.toLowerCase();
+    let mod_text = "";
+
+    if (encrypt) {
+        mod_text = text.replaceAll("e", "enter")
+        .replaceAll("i", "imes")
+        .replaceAll("a", "ai")
+        .replaceAll("o", "ober")
+        .replaceAll("u", "ufat");
+    } else {
+        mod_text = text.replaceAll("enter", "e")
+        .replaceAll("imes", "i")
+        .replaceAll("ai", "a")
+        .replaceAll("ober", "o")
+        .replaceAll("ufat", "u");
+    }
+
+    return mod_text;
+}
+
+
+function encryptText() {
+    let text = document.getElementById("encrypt-text").value;
+    document.getElementById("decrypt-text").value = replacedLetters(text, true);
+}
+
+function decryptText() {
+    let text = document.getElementById("encrypt-text").value;
+    document.getElementById("decrypt-text").value = replacedLetters(text, false);
 }
